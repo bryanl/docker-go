d36bb7f1757eafd6f6ca8b68abee8517835fdeab
root <root@util02.l2.pifft.com>
2014-10-01 16:39:49 -0400

    adding Dockerfile

--- cmd/go-test-app/main.go
+++ cmd/go-test-app/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"github.com/ianschenck/envflag"
 	"net/http"
+	"os"
 )
 
 var (
 	listen = envflag.String("GO_LISTEN", ":8080", "listen")
+	usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		envflag.PrintDefaults()
+	}
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
